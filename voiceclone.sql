26b16415212801a4156fe28542d41577c359ec2e
PATTAN ZAHEER AHMED <113978716+pattanzaheerahmed@users.noreply.github.com>
2024-08-07 13:07:13 +0530

    Add files via upload

---
+++ voiceclone.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Enterprise - MySQL GUI v6.56
+MySQL - 5.0.67-community-nt : Database - voiceclone
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`voiceclone` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `voiceclone`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(1000) default NULL,
+  `email` varchar(1000) default NULL,
+  `mobile` varchar(1000) default NULL,
+  `address` varchar(1000) default NULL,
+  `username` varchar(1000) default NULL,
+  `password` varchar(1000) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`email`,`mobile`,`address`,`username`,`password`) values (1,'venkat','venkat@gmail.com','01234567890','hyd','venkat','123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
